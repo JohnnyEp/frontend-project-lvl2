83c96a5c8668946dedbf0d1ba8b7bee0456e9579
JohnnyEp <mrjohnny2323@gmail.com>
2022-04-02 22:31:03 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -6,22 +6,24 @@ const genDiff = (filepath1, filepath2) => {
   if (!filepath1.endsWith('.json') || !filepath2.endsWith('.json')) {
     return 'One or both files are not .JSON';
   }
-  const file1 = JSON.parse(readFileSync(resolve(filepath1), 'utf-8'));
-  const file2 = JSON.parse(readFileSync(resolve(filepath2), 'utf-8'));
-  const keys = Object.keys({ ...file1, ...file2 });
+  const readFile = (filepath) => readFileSync(resolve(filepath), 'utf-8');
+  const fileData1 = JSON.parse(readFile(filepath1));
+  const fileData2 = JSON.parse(readFile(filepath2));
+  const keys = Object.keys({ ...fileData1, ...fileData2 });
 
   const result = _.sortBy(keys, (key) => key)
     .map((key) => {
-      if (!_.has(file2, key)) {
-        return `- ${key}: ${file1[key]}`; // deleted
+      let temp = '';
+      if (!_.has(fileData2, key)) { // deleted
+        temp = `- ${key}: ${fileData1[key]}`;
+      } else if (!_.has(fileData1, key)) { // added
+        temp = `+ ${key}: ${fileData2[key]}`;
+      } else if (fileData1[key] === fileData2[key]) { // unchanged
+        temp = `  ${key}: ${fileData1[key]}`;
+      } else { // changed
+        temp = `- ${key}: ${fileData1[key]}\n  + ${key}: ${fileData2[key]}`;
       }
-      if (!_.has(file1, key)) {
-        return `+ ${key}: ${file2[key]}`; // added
-      }
-      if (file1[key] === file2[key]) {
-        return `  ${key}: ${file1[key]}`; // unchanged
-      }
-      return `- ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`; // changed
+      return temp;
     })
     .join('\n  ');
 
