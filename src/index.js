d2661e54a9cbe208244da5a5195e67d619b393d3
JohnnyEp <mrjohnny2323@gmail.com>
2022-04-02 22:34:05 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -13,17 +13,16 @@ const genDiff = (filepath1, filepath2) => {
 
   const result = _.sortBy(keys, (key) => key)
     .map((key) => {
-      let temp = '';
-      if (!_.has(fileData2, key)) { // deleted
-        temp = `- ${key}: ${fileData1[key]}`;
-      } else if (!_.has(fileData1, key)) { // added
-        temp = `+ ${key}: ${fileData2[key]}`;
-      } else if (fileData1[key] === fileData2[key]) { // unchanged
-        temp = `  ${key}: ${fileData1[key]}`;
-      } else { // changed
-        temp = `- ${key}: ${fileData1[key]}\n  + ${key}: ${fileData2[key]}`;
+      if (!_.has(fileData2, key)) {
+        return `- ${key}: ${fileData1[key]}`; // deleted
       }
-      return temp;
+      if (!_.has(fileData1, key)) {
+        return `+ ${key}: ${fileData2[key]}`; // added
+      }
+      if (fileData1[key] === fileData2[key]) {
+        return `  ${key}: ${fileData1[key]}`; // unchanged
+      }
+      return `- ${key}: ${fileData1[key]}\n  + ${key}: ${fileData2[key]}`; // changed
     })
     .join('\n  ');
 
