bce45eadb29acced084ea9bb8ad63686eb152bfb
JohnnyEp <mrjohnny2323@gmail.com>
2022-04-17 21:28:47 +0300

    Add the ability to compare YAML files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,32 @@
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
+import { resolve, dirname } from 'path';
 import genDiff from '../src/index.js';
 
-const filepath1 = './__fixtures__/file1.json';
-const filepath2 = './__fixtures__/file2.json';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('genDiff simple', () => {
-  expect(genDiff(filepath1, filepath2)).toEqual(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
+test('wrong ext', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('foo.foo');
+  expect(() => {
+    genDiff(filepath1, filepath2);
+  }).toThrow();
+  expect(() => {
+    genDiff(filepath2, filepath1);
+  }).toThrow();
+});
+
+test('genDiff JSON flat', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  expect(genDiff(filepath1, filepath2)).toEqual(readFile('correct.txt'));
+});
+
+test('genDiff YAML flat', () => {
+  const filepath1 = getFixturePath('file3.yaml');
+  const filepath2 = getFixturePath('file4.yml');
+  expect(genDiff(filepath1, filepath2)).toEqual(readFile('correct.txt'));
 });
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import _ from 'lodash';
-import { resolve } from 'path';
+import { resolve, extname } from 'path';
 import { readFileSync } from 'fs';
+import parserFile from './parsers.js';
 
 const genDiff = (filepath1, filepath2) => {
-  if (!filepath1.endsWith('.json') || !filepath2.endsWith('.json')) {
-    return 'One or both files are not .JSON';
-  }
+  const ext1 = extname(filepath1);
+  const ext2 = extname(filepath2);
   const readFile = (filepath) => readFileSync(resolve(filepath), 'utf-8');
-  const fileData1 = JSON.parse(readFile(filepath1));
-  const fileData2 = JSON.parse(readFile(filepath2));
+  const fileData1 = parserFile(readFile(filepath1), ext1);
+  const fileData2 = parserFile(readFile(filepath2), ext2);
   const keys = Object.keys({ ...fileData1, ...fileData2 });
 
   const result = _.sortBy(keys, (key) => key)
